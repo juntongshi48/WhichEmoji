1d9a670a8a2e16773881de6a9d242455a55cb8fd
juntongshi48 <shisteve@usc.edu>
2023-12-09 03:13:51 +0000

    code reorganized + argparse + new config path

--- experiments/train_rnn.mk
+++ experiments/train_rnn.mk
@@ -7,7 +7,6 @@ train_rnn:
 	$(eval EPOCHS := 10)
 	$(eval LR := 0.0005)
 	${eval CONFIG_FILE := configs/rnn.yaml}
-	-git add .; git commit -m "commit to run"
 	${eval REVISION := ${shell git rev-parse --short HEAD}}
 	CUDA_VISIBLE_DEVICES=${GPU_ID} python core/main.py --config_file ${CONFIG_FILE} \
 		--batch_size ${BATCH_SIZE} --epochs ${EPOCHS} --lr ${LR} 
