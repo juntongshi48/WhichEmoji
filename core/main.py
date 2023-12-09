85037286eb953986d242faefd463b5804a4aae32
juntongshi48 <shisteve@usc.edu>
2023-12-09 01:52:05 +0000

    commit to run

--- core/main.py
+++ core/main.py
@@ -159,8 +159,8 @@ def main(cfg):
     # train_args['device'] = 'cuda:1'
 
     # train_args['num_class'] = len(id2label) # TODO: connect this to preprocessing
-    # cfg.__dict__["num_class"] = len(id)
-    cfg.num_class = len(id)
+    cfg.__dict__["num_class"] = len(id)
+    # cfg.num_class = len(id)
     pdb.set_trace()
 
     train_path = "core/dataset/data/processed/train.csv"
