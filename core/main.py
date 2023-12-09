e932ccf54c5206df15dde4c17a65be7886503a59
juntongshi48 <shisteve@usc.edu>
2023-12-09 03:00:58 +0000

    commit to run

--- core/main.py
+++ core/main.py
@@ -199,11 +199,11 @@ def main(cfg):
     att = "att" if cfg.model.attention else "noatt"
     # /core/results/confusion_matrix/
     filename = f'confusion_matrix/CM\
-    _demb{cfg.model.d_embedding}\
-    _dhid{cfg.model.d_hidden}\
-    _nlay{cfg.model.n_layer}\
-    _bs{cfg.batch_size}\
-    _{att}.png'
+_demb{cfg.model.d_embedding}\
+_dhid{cfg.model.d_hidden}\
+_nlay{cfg.model.n_layer}\
+_bs{cfg.batch_size}\
+_{att}.png'
     plot_confusion_matrix(id2label.values(), test_metrics_best['confusion_matrix'], 'Confusion_Matrix', filename)
 
 
